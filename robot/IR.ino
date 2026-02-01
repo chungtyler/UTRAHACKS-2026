25479351ed117669ea1d46a52a2e476fa050612c
Sujash Nayak <sujash.nayak@gmail.com>
2026-01-31 21:47:41 -0500

    hello

--- robot/IR.ino
+++ robot/IR.ino
@@ -1,14 +1,16 @@
 int IRL = A1;
 int IRR = A0;
 
-int IRL_threshold = 0;
-int IRR_threshold = 0;
+int IRL_threshold = 512;
+int IRR_threshold = 512;
 
 
 void IR_read() {
   IRL_signal = analogRead(IRL);//digitalRead(IRL);
+  Serial.println(IRL_signal);
   IRL_signal = convert_to_binary(IRL_signal, IRL_threshold);
   IRR_signal = analogRead(IRR);//digitalRead(IRR);
+  Serial.println(IRR_signal);
   IRR_signal = convert_to_binary(IRR_signal, IRR_threshold);
 }
 
